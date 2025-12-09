0eecd5f743690baa3c0ee571256d232eb143ee54
Elena050 <2008petrova.elena@gmail.com>
2025-12-09 19:24:20 +0300

    Update IAttemptRepository.cs

--- IAttemptRepository.cs
+++ IAttemptRepository.cs
@@ -1,1 +1,17 @@
+using dtos;
+using Request.Attempt;
 
+namespace Repositories
+{
+    public interface IAttemptRepository
+    {
+        Task<AttemptDto?> GetByIdAsync(int id);
+        Task<IAttemptDto> CreateAsync(StartAttemptRequest requests);
+        Task<AttemptDto> UpdateAsync(int id, UpdateAttemptRequest requests);
+        Task<IEnumerable<AttemptDto>> GatByStudentIdAsync(int studentId);
+        Task<IEnumerable<AttemptDto>> GetByTestIdAsync(int testId);
+        Task<bool> CanStudentStartAttemptAsync(int studentId, int testId);
+        Task<bool> ExistsAsync(int Id);
+        Task,int> GetAttemptsCountAsync(int studentId, int testId);
+    }
+}
